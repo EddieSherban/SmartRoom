eb0fd4530578bf0463b025b032123852bcd5a458
EddieSherban <eddiesrbn3@gmail.com>
2024-06-15 20:43:52 -0700

    configured project

---
+++ firmware/window/window-control/include/mqtt_network.h
@@ -0,0 +1,22 @@
+//mqtt_network.h
+
+#include "mqtt_client.h"
+#include <stdio.h>
+
+#ifndef MQTT_NETWORK_H
+#define MQTT_NETWORK_H
+
+namespace MyNamespace {
+    class MyClass {
+    public:
+        MyClass(); // Constructor
+        ~MyClass(); // Destructor
+        
+        void doSomething(); // Member function
+        
+    private:
+        int myData; // Member variable
+    };
+}
+
+#endif //MQTT_NETWORK_H
---
+++ firmware/window/window-control/main/main.cpp
@@ -0,0 +1,50 @@
+/******************************************************************************
+
+    Project: Smart Room
+    Description: This project contains functions for Smart Room tasks
+                 such as window control.
+
+    Author: Eddie Sherban
+    Date: 2024-06-14
+
+    Files:
+    - main.cpp
+    - motor_control.c
+    - mqtt_network.c
+
+******************************************************************************/
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+
+#define CONFIG_BLINK_PERIOD 1000
+
+static const char* TAG = "main";
+static gpio_num_t LED_PIN = static_cast<gpio_num_t>(2);
+static uint32_t LED_state = 0;
+
+void configure_led(void) {
+    ESP_LOGI(TAG, "configuring LED...");
+    gpio_reset_pin(LED_PIN);
+    gpio_mode_t mode = GPIO_MODE_OUTPUT;
+    gpio_set_direction(LED_PIN, mode);
+}
+
+static void blink_led(void) {
+    gpio_set_level(LED_PIN, LED_state);
+}
+
+extern "C" void app_main(void)
+{
+    configure_led();
+
+    while(1) {
+        ESP_LOGI(TAG, "Turning the LED %s", LED_state == true ? "ON": "OFF");
+        blink_led();
+        LED_state = !LED_state;
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+    }
+}
